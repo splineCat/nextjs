bba835c72b810a465b98bdc8fd593d70a6db04a2
splineCat <splinecat@gmail.com>
2024-06-29 12:31:26 +0900

    fit<P>

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <div>안녕하세요</div>
+        {children}
+      </body>
     </html>
   );
 }
